2bd96af9706645fd4ef9ecd1396a4b2b120cb6e5
Richard Sustek <ennspassion@gmail.com>
2025-06-03 10:32:27 +0200

    reformats with tab size

--- tests/integration-tests.config.ts
+++ tests/integration-tests.config.ts
@@ -10,34 +10,94 @@ const integrationEnv = {
 
 export function getIntegrationTestConfig() {
 	const languageVariantUrl = (itemCodename: string, languageCodename: string) =>
-		getMapiEndpointUrl({ environmentId: integrationEnv.id, path: `/items/codename/${itemCodename}/variants/codename/${languageCodename}` });
+		getMapiEndpointUrl({
+			environmentId: integrationEnv.id,
+			path: `/items/codename/${itemCodename}/variants/codename/${languageCodename}`,
+		});
 
 	return {
 		environmentId: integrationEnv.id,
 		mapiKey: integrationEnv.mapiKey,
 		mapiUrls: {
-			contentType: (codename: string) => getMapiEndpointUrl({ environmentId: integrationEnv.id, path: `/types/codename/${codename}` }),
-			taxonomy: (codename: string) => getMapiEndpointUrl({ environmentId: integrationEnv.id, path: `/taxonomies/codename/${codename}` }),
-			contentItem: (codename: string) => getMapiEndpointUrl({ environmentId: integrationEnv.id, path: `/items/codename/${codename}` }),
-			language: (codename: string) => getMapiEndpointUrl({ environmentId: integrationEnv.id, path: `/languages/codename/${codename}` }),
+			contentType: (codename: string) =>
+				getMapiEndpointUrl({
+					environmentId: integrationEnv.id,
+					path: `/types/codename/${codename}`,
+				}),
+			taxonomy: (codename: string) =>
+				getMapiEndpointUrl({
+					environmentId: integrationEnv.id,
+					path: `/taxonomies/codename/${codename}`,
+				}),
+			contentItem: (codename: string) =>
+				getMapiEndpointUrl({
+					environmentId: integrationEnv.id,
+					path: `/items/codename/${codename}`,
+				}),
+			language: (codename: string) =>
+				getMapiEndpointUrl({
+					environmentId: integrationEnv.id,
+					path: `/languages/codename/${codename}`,
+				}),
 			languageVariant: languageVariantUrl,
 			publish: (itemCodename: string, languageCodename: string) => `${languageVariantUrl(itemCodename, languageCodename)}/publish`,
-			contentTypes: getMapiEndpointUrl({ environmentId: integrationEnv.id, path: '/types' }),
-			taxonomies: getMapiEndpointUrl({ environmentId: integrationEnv.id, path: '/taxonomies' }),
-			contentItems: getMapiEndpointUrl({ environmentId: integrationEnv.id, path: '/items' }),
+			contentTypes: getMapiEndpointUrl({
+				environmentId: integrationEnv.id,
+				path: '/types',
+			}),
+			taxonomies: getMapiEndpointUrl({
+				environmentId: integrationEnv.id,
+				path: '/taxonomies',
+			}),
+			contentItems: getMapiEndpointUrl({
+				environmentId: integrationEnv.id,
+				path: '/items',
+			}),
 		},
 		deliveryUrls: {
-			contentType: (codename: string) => getDeliveryEndpointUrl({ environmentId: integrationEnv.id, path: `/types/${codename}` }),
-			taxonomy: (codename: string) => getDeliveryEndpointUrl({ environmentId: integrationEnv.id, path: `/taxonomies/${codename}` }),
-			contentItem: (codename: string) => getDeliveryEndpointUrl({ environmentId: integrationEnv.id, path: `/items/${codename}` }),
+			contentType: (codename: string) =>
+				getDeliveryEndpointUrl({
+					environmentId: integrationEnv.id,
+					path: `/types/${codename}`,
+				}),
+			taxonomy: (codename: string) =>
+				getDeliveryEndpointUrl({
+					environmentId: integrationEnv.id,
+					path: `/taxonomies/${codename}`,
+				}),
+			contentItem: (codename: string) =>
+				getDeliveryEndpointUrl({
+					environmentId: integrationEnv.id,
+					path: `/items/${codename}`,
+				}),
 		},
 	};
 }
 
-export function getMapiEndpointUrl({ environmentId, path }: { readonly environmentId: string; readonly path: string }): string {
-	return getEndpointUrl({ environmentId, path, baseUrl: integrationEnv.mapiBaseUrl ?? 'https://manage.kontent.ai/v2/projects/' });
+export function getMapiEndpointUrl({
+	environmentId,
+	path,
+}: {
+	readonly environmentId: string;
+	readonly path: string;
+}): string {
+	return getEndpointUrl({
+		environmentId,
+		path,
+		baseUrl: integrationEnv.mapiBaseUrl ?? 'https://manage.kontent.ai/v2/projects/',
+	});
 }
 
-export function getDeliveryEndpointUrl({ environmentId, path }: { readonly environmentId: string; readonly path: string }): string {
-	return getEndpointUrl({ environmentId, path, baseUrl: integrationEnv.deliveryBaseUrl ?? 'https://delivery.kontent.ai/' });
+export function getDeliveryEndpointUrl({
+	environmentId,
+	path,
+}: {
+	readonly environmentId: string;
+	readonly path: string;
+}): string {
+	return getEndpointUrl({
+		environmentId,
+		path,
+		baseUrl: integrationEnv.deliveryBaseUrl ?? 'https://delivery.kontent.ai/',
+	});
 }
